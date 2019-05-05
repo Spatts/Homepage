db4e3262d37dbc8350308025de4e1d9d2a043511
Steven <mitchs.patterson@gmail.com>
2019-05-04 20:15:18 -0600

    new

--- web/homepage.php
+++ web/homepage.php
@@ -5,13 +5,21 @@
     <meta name="viewport" content="width=s, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
 </head>
 <body>
-    <header>White Ranger</header>
+    <div class="jumbotron text-center">
+        <h1>White Ranger</h1>
+    </div>
     <img src="whiteranger.jpg" alt="white ranger">
 
-    <?php 
-        echo date("l jS \of F Y h:i:s A") . "<br>";
-    ?>
+    <br>
+    <footer>
+        <?php 
+            date_default_timezone_set('America/Denver');
+            echo date("l jS \of F Y h:i:s A") . "<br>";
+        ?>
+    </footer>
 </body>
 </html>
