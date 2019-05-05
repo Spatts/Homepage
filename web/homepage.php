13326bb1c7012b7b4bcb11e799d50d539332e5d4
Steven <mitchs.patterson@gmail.com>
2019-05-04 19:39:14 -0600

    basics

---
+++ web/homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+<body>
+<header>White Ranger</header>
+<img src="whiteranger" alt="white ranger">
+    <?php 
+    
+    ?>
+</body>
+</html>
